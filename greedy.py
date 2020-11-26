ead18d6474fa51d68d38c0a02b338feebac1aa84
raymond-Gwon <73869660+raymond-Gwon@users.noreply.github.com>
2020-11-26 23:10:29 +0900

    Update greedy.py

--- greedy.py
+++ greedy.py
@@ -1,10 +1,14 @@
+from random import *
+
 # -- 거스름돈 문제 --
 
-n = 1260
+n = randint(1, 1000) * 10
 count = 0
+coin_array = [500, 100, 50, 10]
+
+print(n)
 
 # 큰 단위의 화폐부터 차례대로 확인하기
-coin_array = [500, 100, 50, 10]
 
 for coin in coin_array:
     count += n // coin
